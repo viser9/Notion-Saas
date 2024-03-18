0f2f7470893dc8d14e5a6a6a1223ed48567fc656
Vikas Swain <vikas@Vikass-MacBook-Air.local>
2024-03-19 01:15:40 +0530

    schema written and database setup

--- src/supabase/db.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import * as dotenv from 'dotenv';
-import * as schema from '../../../../';
-dotenv.config({ path: '.env' });
-
-if (!process.env.DATABASE_URL) {
-    console.log()
-}
